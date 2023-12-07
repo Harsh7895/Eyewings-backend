7e610ecc47a0cc5399c3bd33e1bb0199b1ee1b64
Harsh Pawar <harshsik7890@gmail.com>
2023-12-07 17:47:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express()
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:3000" ,
+    origin: "https://eyewings-frontend.vercel.app" ,
     methods: 'GET , POST , PUT , PATCH , DELETE , HEAD',
     credentials: true
 }))
